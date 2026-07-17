b5400ed173aaf1543293510795884580143e445d
Nsstr <tartalonestor@gmail.com>
2026-07-17 14:34:16 -0300

    Convert app to PWA

---
+++ horarios-app/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'retail-plan-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './favicon.png',
+  './admin-permissions.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          // Si falla la red y no está en caché, intentamos devolver el index.html para que no se rompa la PWA
+          if (event.request.mode === 'navigate') {
+            return caches.match('./index.html');
+          }
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
